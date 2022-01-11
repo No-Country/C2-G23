9176c77885a3476d1463c3921a3c090e2daa796f
CanCerber0 <geronimo.bknuttzen@gmail.com>
2022-01-11 03:06:53 -0300

    Comienzo del proyecto desde el Front

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import Footer from './components/Footer';
 import Header from './components/Header';
-import Body from "./components/Body";
+import Home from './components/Home';
+import Search from './components/Search';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="app container">
+    <Router>
         <Header />
-        <Body />
-    </div>
+        <Routes>
+          <Route path="/search" element={<Search />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+    </Router>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react'
+import { AppBar, makeStyles, Toolbar } from "@material-ui/core";
 
-class Header extends React.Component {
-    
-    render() { 
-        return (
-            <form>
-            </form>
-            ) 
-            
+const Header = () => {
+    const[mobile, setMobile] = useState(false)
+    const classes = UseStyle()
+    const displayMobile = () =>{ }
+    const displayDesktop = () =>{
+        <Toolbar className={classes.toolbar}>
+
+        </Toolbar>
     }
+    return (
+        <AppBar>
+            <p>HEADER</p>
+            {
+                mobile ? displayMobile() : displayDesktop()
+            }
+        </AppBar>
+    )
 }
- 
+const UseStyle = makeStyles((theme)=>({
+    toolbar:{
 
-export default Header;
+    }
+}))
+
+export default Header
