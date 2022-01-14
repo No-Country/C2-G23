b7bb74797c9f223fd60ae179e2610ed022517d3d
Emanuel Cruz <emanuelcruz091@gmail.com>
2022-01-14 18:12:53 -0300

    Importo libreria para consulta de hotel

--- api/app/Repositories/HotelInformationRepository.php
+++ api/app/Repositories/HotelInformationRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\HotelInformation;
 use App\Repositories\Interface\RepositoryInterface;
+use Illuminate\Support\Facades\DB;
 
 class HotelInformationRepository implements RepositoryInterface {
 
@@ -36,7 +37,7 @@ class HotelInformationRepository implements RepositoryInterface {
     }
 
     public function showHotelsByZoneLocation(string $province, string $city) {
-        $hotelInformation = DB::table('hotel')
+        return DB::table('hotel')
             ->leftJoin('hotel_information', 'hotel_information_id', '=', 'hotel.hotel_information_id')
             ->leftJoin('zone_location', 'zone_location.id', '=', 'hotel.zone_location_id')
             ->leftJoin('province', 'province.id', '=', 'zone_location.province_id')
@@ -50,7 +51,5 @@ class HotelInformationRepository implements RepositoryInterface {
             ->where('province.name','',$province)
             ->where('city.name','',$city)
             ->get();
-
-        return $hotelInformation;
     }
 }
