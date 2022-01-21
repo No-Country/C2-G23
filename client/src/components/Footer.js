a3b3193dd1ad312913a80849854c4cc5819d5208
mnperrone <mnperrone@gmail.com>
2022-01-20 22:27:54 -0300

    actualización

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+import styles from "../styles/Footer.module.css"
+
+const Footer = () => {
+    return (
+        /* <p className={styles.p}>ACÁ VA EL FOOTER</p> */
+        <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"/>
+      Bootstrap
+    </a>
+  </div>
+</nav>
+    )
+}
+
+export default Footer
